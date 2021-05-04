69bf4f955abad0ebeb220de7bc8266db6807b7c1
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-04 12:24:22 +0200

    3.2. Correct the logic of two login methods.

--- main.py
+++ main.py
@@ -27,11 +27,15 @@ def post_login_session(response: Response, credentials: HTTPBasicCredentials = D
             headers={"WWW-Authenticate": "Basic"},
         )
     response.set_cookie(key="session_token", value=S_TOKEN)
-    return credentials.username
 
 @app.post("/login_token", status_code=201)
-def post_login_token(*, response: Response, session_token: str = Cookie(None)):
-    if session_token != S_TOKEN:
-        raise HTTPException(status_code=401, detail="Unathorised")
-    else:
-        return {"token": S_TOKEN}
+def post_login_token(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+    correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return {"token": S_TOKEN}
